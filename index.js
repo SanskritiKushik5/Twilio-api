a1d5c97983f44904d79f459f71b596844ab9c567
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-08-29 14:45:39 +0530

    Whatsapp integration-1

--- index.js
+++ index.js
@@ -11,7 +11,26 @@ app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
     res.render("index");
-  });
+});
+
+app.post("/whatsapp", (req, res) => {
+    const mobile = req.body.mobile;
+    const client = require("twilio")(process.env.accountSid, process.env.authToken);
+    client.messages 
+      .create({ 
+         body: 'Hello👋! This is Sanskriti 😎... Let\'s catch up someday?', 
+         from: 'whatsapp:+14155238886',       
+         to: `whatsapp:${mobile}`
+       }) 
+      .then(message => {
+        console.log(message.sid);
+        res.redirect("/");
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .done();
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Started on PORT: " + PORT));
